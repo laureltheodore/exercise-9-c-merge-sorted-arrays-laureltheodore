644278ad5f9f1265ccd14bfbff84d56cab6044bf
laureltheodore <110010515+laureltheodore@users.noreply.github.com>
2022-10-11 12:10:22 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,53 @@
 from typing import List
 
 def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-  # Write code here
+ 
+  x = nums1[0:m]
+  y = nums2[0:n]
+  nums1=x+y
+  nums1 = merge_sort(nums1)
+  return nums1
+  
+def merge_sort(myList) -> None:
+  if len(myList) > 1:
+        mid = len(myList) // 2
+        left = myList[:mid]
+        right = myList[mid:]
+
+        # Recursive call on each half
+        merge_sort(left)
+        merge_sort(right)
+
+        # Two iterators for traversing the two halves
+        i = 0
+        j = 0
+        
+        # Iterator for the main list
+        k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+              # The value from the left half has been used
+              myList[k] = left[i]
+              # Move the iterator forward
+              i += 1
+            else:
+                myList[k] = right[j]
+                j += 1
+            # Move to the next slot
+            k += 1
+
+        # For all the remaining values
+        while i < len(left):
+            myList[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            myList[k]=right[j]
+            j += 1
+            k += 1
+  return myList
 
 
 # Do not change the following code
@@ -13,5 +59,5 @@ for item in input().split(', '):
   nums2.append(int(item))
 m = int(input())
 n = int(input())
-merge(nums1, m, nums2, n)
-print(nums1)
+
+print(merge(nums1, m, nums2, n))
