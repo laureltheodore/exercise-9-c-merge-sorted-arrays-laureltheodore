c05f5118e54b7962808524ee197def2f80044f3e
laureltheodore <110010515+laureltheodore@users.noreply.github.com>
2022-10-11 12:00:13 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
   x = nums1[0:m]
   y = nums2[0:n]
   nums1=x+y
-  nums1 = merge_sort(nums1)
+  nums1 = merge_sort(nums1) 
   return nums1
   
 def merge_sort(myList) -> None:
